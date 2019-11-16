753be2db56ddbd12f2ce848f96bb725a2af9576e
Zorden <valentin.pro@inbox.ru>
2019-11-16 23:19:04 +0300

    в работе

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{info}}
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      info: ''
+    }
+  },
+  mounted () {
+    this.$store.dispatch('')
+  }
+}
+</script>
